56f7b63c7d1ed563dafb16c6c8794aa3b66cb490
raaamonnn <46769373+raaamonnn@users.noreply.github.com>
2019-09-15 14:43:24 -0700

    Add files via upload

---
+++ BinaryTreeSearch.h
@@ -0,0 +1,201 @@
+#ifndef BINARYTREESEARCH_H_
+#define BINARYTREESEARCH_H_
+
+#include "header.h"
+#include <queue>
+class BinaryTreeSearch
+{
+private:
+	struct TreeNode
+	{
+		string value;
+		TreeNode *left;
+		TreeNode *right;
+
+		TreeNode(string value, TreeNode *left = nullptr, TreeNode *right = nullptr)
+		{
+			this->value = value;
+			this->left = left;
+			this->right = right;
+		}
+	};
+
+	TreeNode *root;
+
+	//inserting values
+	void insert(TreeNode *&tree, string value)
+	{
+		//if the tree is empty BASE CASE 1
+		if(tree == nullptr)
+		{
+		tree = new TreeNode(value);
+		return;
+		}
+
+		//if the value is already the tree BASE CASE 2
+		if(value == tree->value)
+		{
+			cout << value << " already exists in the tree\n";
+			return;
+		}
+
+		//if the value is being inserted is less than the value of the current node
+		if(value < tree->value)
+		{
+			insert(tree->left, value);
+		}
+		//if the value is greater
+		else
+		{
+			insert(tree->right,value);
+		}
+
+	}
+
+	void displayInOrder(TreeNode *tree)
+	{
+		//if the tree is empty BASE CASE 1
+		if(tree==nullptr)
+		{
+			return;
+		}
+		else
+		{
+			displayInOrder(tree->left);
+			cout << tree->value << " ";
+			displayInOrder(tree->right);
+		}
+	}
+
+public:
+	BinaryTreeSearch()
+	{
+		root = nullptr;
+	}
+
+	//this is done so the user does not have direct access to root
+	void insert(string value)
+	{
+		insert(root, value);
+	}
+
+	void showInOrder()
+	{
+		displayInOrder(root);
+	}
+
+	void search()
+	{
+		//ask user for the letter
+		//if letter is in the tree, then return a found message
+		TreeNode *tree = root;
+		string userInput;
+		bool found = false;
+
+		//prompt user
+		cout << "\nPlease enter a value that you would like to search for: ";
+		getline(cin, userInput);
+
+
+		//search for the letter
+		while(tree!=nullptr)
+		{
+			if(tree->value == userInput)
+			{
+				found = true;
+				break;
+			}
+			else if(userInput < tree->value)
+			{
+				tree = tree->left;
+			}
+			else
+			{
+				tree = tree->right;
+			}
+		}
+
+		if(found)
+		{
+			cout << "The value " << userInput << " has been found in the tree!\n";
+		}
+		else
+		{
+			cout << "The value " << userInput << " was not found!\n";
+		}
+	}
+
+	void printPostorder(TreeNode* tree)
+	{
+	    if (tree == NULL)
+	        return;
+
+	    // first recur on left subtree
+	    printPostorder(tree->left);
+
+	    // then recur on right subtree
+	    printPostorder(tree->right);
+
+	    // now deal with the node
+	    cout << tree->value << " ";
+	}
+
+	/* Given a binary tree, print its nodes in inorder*/
+	void printInorder(TreeNode* tree)
+	{
+	    if (tree == NULL)
+	        return;
+
+	    /* first recur on left child */
+	    printInorder(tree->left);
+
+	    /* then print the data of node */
+	    cout << tree->value << " ";
+
+	    /* now recur on right child */
+	    printInorder(tree->right);
+	}
+
+	/* Given a binary tree, print its nodes in preorder*/
+	void printPreorder(TreeNode* tree)
+	{
+	    if (tree == NULL)
+	        return;
+
+	    /* first print data of node */
+	    cout << tree->value << " ";
+
+	    /* then recur on left sutree */
+	    printPreorder(tree->left);
+
+	    /* now recur on right subtree */
+	    printPreorder(tree->right);
+	}
+
+	void printFirstBreadth(TreeNode* tree)
+	{
+	    queue<TreeNode> queue;
+	    TreeNode * traversal;
+
+	    queue.push(tree->value); //needed to make the while loop work
+
+	    while(!queue.empty())
+	    {
+	    	traversal = &queue.front();
+	    	queue.pop();
+
+	    	if(traversal->left!=nullptr)
+	    	{
+	    		queue.push(traversal->left->value);
+	    	}
+
+	    	if(traversal->right!=nullptr)
+	    	{
+	    		queue.push(traversal->right->value);
+	    	}
+	    }
+	}
+};
+
+
+#endif /* BINARYTREESEARCH_H_ */
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+#include "BinaryTreeSearch.h"
+int main()
+{
+	BinaryTreeSearch tree;
+	tree.insert("A");
+	tree.insert("B");
+
+	tree.printInorder();
+return 0;
+}
